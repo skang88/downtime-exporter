291215672dd449db2283464227c15a3f50d56206
Seokgyun Kang <seokgyun.kang@seohanga.com>
2025-10-21 09:21:26 -0400

    timezone trouble shooting

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    timezone: 'Z'
 };
 
 const port = process.env.PORT || 8002;
